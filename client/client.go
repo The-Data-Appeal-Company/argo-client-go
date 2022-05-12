d9d8be40d76e2f8ab77c58a42b07e5cdc430e078
GaruGaru <garuglieritommaso.dev@gmail.com>
2022-05-12 16:49:57 +0200

    initial commit

---
+++ client/client.go
@@ -0,0 +1,87 @@
+package client
+
+// package "github.com/The-Data-Appeal-Company/argo-client-go/client"
+import (
+	"github.com/argoproj/argo-workflows/v3/pkg/apiclient"
+	"github.com/argoproj/argo-workflows/v3/pkg/apiclient/workflow"
+	"github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
+	"time"
+)
+
+type Argo struct {
+	client      workflow.WorkflowServiceClient
+	pollingTime time.Duration
+}
+
+type Opts struct {
+	pollingTime time.Duration
+}
+
+type CreateRequest struct {
+	Namespace string             `json:"namespace" yaml:"namespace"`
+	Workflow  *v1alpha1.Workflow `json:"workflow" yaml:"workflow"`
+}
+
+type GetRequest struct {
+	Namespace string `json:"namespace" yaml:"namespace"`
+	Name      string `json:"name" yaml:"name"`
+}
+
+func New(client workflow.WorkflowServiceClient, opts Opts) *Argo {
+	return &Argo{
+		client:      client,
+		pollingTime: opts.pollingTime,
+	}
+}
+
+func NewFromArgoServer(url string, opts Opts) (*Argo, error) {
+	_, client, err := apiclient.NewClientFromOpts(apiclient.Opts{
+		ArgoServerOpts: apiclient.ArgoServerOpts{
+			URL: url,
+		},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return New(client.NewWorkflowServiceClient(), opts), nil
+}
+
+func (a *Argo) Client() workflow.WorkflowServiceClient {
+	return a.client
+}
+
+func (a *Argo) CreateWorkflow(ctx context.Context, req CreateRequest) (*v1alpha1.Workflow, error) {
+	return a.client.CreateWorkflow(ctx, &workflow.WorkflowCreateRequest{
+		Namespace: req.Namespace,
+		Workflow:  req.Workflow,
+	})
+}
+
+func (a *Argo) GetWorkflow(ctx context.Context, req GetRequest) (*v1alpha1.Workflow, error) {
+	return a.client.GetWorkflow(ctx, &workflow.WorkflowGetRequest{
+		Name:      req.Name,
+		Namespace: req.Namespace,
+	})
+}
+
+func (a *Argo) WaitWorkflow(ctx context.Context, req GetRequest) (*v1alpha1.Workflow, error) {
+	tk := time.NewTimer(a.pollingTime)
+	for {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		case <-tk.C:
+			wf, err := a.GetWorkflow(ctx, req)
+			if err != nil {
+				return nil, err
+			}
+
+			if wf.Status.Phase.Completed() {
+				tk.Stop()
+				return wf, nil
+			}
+		}
+	}
+}
---
+++ client/client_test.go
@@ -0,0 +1,181 @@
+package client
+
+import (
+	"github.com/argoproj/argo-workflows/v3/pkg/apiclient/workflow/mocks"
+	"github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/net/context"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"testing"
+	"time"
+)
+
+func TestArgo_CreateWorkflow(t *testing.T) {
+	expectedWf := v1alpha1.Workflow{
+		TypeMeta: v1.TypeMeta{},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "test-workflow-00",
+			Namespace: "default",
+		},
+		Status: v1alpha1.WorkflowStatus{
+			Phase: v1alpha1.WorkflowPending,
+		},
+	}
+
+	client := &mocks.WorkflowServiceClient{}
+	client.On("CreateWorkflow", mock.Anything, mock.Anything).Return(&expectedWf, nil)
+
+	argo := New(client, Opts{pollingTime: 100 * time.Millisecond})
+
+	wf, err := argo.CreateWorkflow(context.TODO(), CreateRequest{
+		Namespace: "default",
+		Workflow:  &expectedWf,
+	})
+
+	require.NoError(t, err)
+	require.NotNil(t, wf)
+
+	require.Equal(t, expectedWf.Name, wf.Name)
+	require.Equal(t, expectedWf.Namespace, wf.Namespace)
+	require.Equal(t, expectedWf.Status.Phase, wf.Status.Phase)
+}
+
+func TestArgo_GetWorkflow(t *testing.T) {
+	expectedWf := v1alpha1.Workflow{
+		TypeMeta: v1.TypeMeta{},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "test-workflow-00",
+			Namespace: "default",
+		},
+		Status: v1alpha1.WorkflowStatus{
+			Phase: v1alpha1.WorkflowPending,
+		},
+	}
+
+	client := &mocks.WorkflowServiceClient{}
+
+	client.On("GetWorkflow", mock.Anything, mock.Anything).Return(&expectedWf, nil)
+
+	argo := New(client, Opts{pollingTime: 100 * time.Millisecond})
+
+	wf, err := argo.GetWorkflow(context.TODO(), GetRequest{
+		Namespace: "default",
+		Name:      "test-job-00",
+	})
+
+	require.NoError(t, err)
+	require.NotNil(t, wf)
+	require.Equal(t, expectedWf.Name, wf.Name)
+	require.Equal(t, expectedWf.Namespace, wf.Namespace)
+	require.Equal(t, expectedWf.Status.Phase, wf.Status.Phase)
+}
+
+func TestArgo_WaitWorkflow(t *testing.T) {
+	expectedWf := v1alpha1.Workflow{
+		TypeMeta: v1.TypeMeta{},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "test-workflow-00",
+			Namespace: "default",
+		},
+		Status: v1alpha1.WorkflowStatus{
+			Phase: v1alpha1.WorkflowSucceeded,
+		},
+	}
+
+	client := &mocks.WorkflowServiceClient{}
+	client.On("GetWorkflow", mock.Anything, mock.Anything).Return(&expectedWf, nil)
+
+	argo := New(client, Opts{
+		pollingTime: 100 * time.Millisecond,
+	})
+
+	wf, err := argo.WaitWorkflow(context.TODO(), GetRequest{
+		Namespace: "default",
+		Name:      "test-job-00",
+	})
+
+	require.NoError(t, err)
+	require.NotNil(t, wf)
+
+	require.Equal(t, expectedWf.Name, wf.Name)
+	require.Equal(t, expectedWf.Namespace, wf.Namespace)
+	require.Equal(t, expectedWf.Status.Phase, wf.Status.Phase)
+}
+
+func TestArgo_WaitWorkflow_Pending(t *testing.T) {
+	expectedWf := v1alpha1.Workflow{
+		TypeMeta: v1.TypeMeta{},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "test-workflow-00",
+			Namespace: "default",
+		},
+		Status: v1alpha1.WorkflowStatus{
+			Phase: v1alpha1.WorkflowPending,
+		},
+	}
+
+	client := &mocks.WorkflowServiceClient{}
+	client.On("GetWorkflow", mock.Anything, mock.Anything).Return(&expectedWf, nil)
+
+	argo := New(client, Opts{
+		pollingTime: 100 * time.Millisecond,
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+
+	defer cancel()
+
+	_, err := argo.WaitWorkflow(ctx, GetRequest{
+		Namespace: "default",
+		Name:      "test-job-00",
+	})
+
+	require.Error(t, err)
+}
+
+func TestArgo_WaitWorkflow_SwitchPendingToRunning(t *testing.T) {
+	expectedWf := v1alpha1.Workflow{
+		TypeMeta: v1.TypeMeta{},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "test-workflow-00",
+			Namespace: "default",
+		},
+		Status: v1alpha1.WorkflowStatus{
+			Phase: v1alpha1.WorkflowPending,
+		},
+	}
+
+	completedWf := v1alpha1.Workflow{
+		TypeMeta: v1.TypeMeta{},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "test-workflow-00",
+			Namespace: "default",
+		},
+		Status: v1alpha1.WorkflowStatus{
+			Phase: v1alpha1.WorkflowSucceeded,
+		},
+	}
+
+	client := &mocks.WorkflowServiceClient{}
+
+	// after first call switch the jobs status phase in order to terminate the polling
+	client.On("GetWorkflow", mock.Anything, mock.Anything).
+		Return(&expectedWf, nil).
+		Return(&completedWf, nil)
+
+	argo := New(client, Opts{
+		pollingTime: 100 * time.Millisecond,
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+
+	defer cancel()
+
+	_, err := argo.WaitWorkflow(ctx, GetRequest{
+		Namespace: "default",
+		Name:      "test-job-00",
+	})
+
+	require.NoError(t, err)
+}
